8b08d36c383b199cb60d6053cd7cfa5dcd5f0109
“li-peifeng” <peifeng@me.com>
2024-03-12 19:08:58 +0100

    0313

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -17,7 +17,7 @@ const Index = () => {
         }}
       >
         <I18nContext.Provider value={i18n}>
-          <NotificationsProvider duration={300000}>
+          <NotificationsProvider duration={3000}>
             <Suspense fallback={<FullScreenLoading />}>
               <App />
             </Suspense>
