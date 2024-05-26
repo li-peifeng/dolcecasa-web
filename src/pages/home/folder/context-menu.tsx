45b5e90c17cc6b060b2abcb98a3ee1ec7cdd5f12
“li-peifeng” <peifeng@me.com>
2024-05-26 17:28:20 +0200

    sweet

--- src/pages/home/folder/context-menu.tsx
+++ src/pages/home/folder/context-menu.tsx
@@ -1,12 +1,14 @@
-import { Menu, Item } from "solid-contextmenu"
-import { useCopyLink, useDownload, useT } from "~/hooks"
+import { Menu, Item, Submenu } from "solid-contextmenu"
+import { useCopyLink, useDownload, useLink, useT } from "~/hooks"
 import "solid-contextmenu/dist/style.css"
-import { HStack, Icon, Text, useColorMode } from "@hope-ui/solid"
+import { HStack, Icon, Text, useColorMode, Image } from "@hope-ui/solid"
 import { operations } from "../toolbar/operations"
 import { For, Show } from "solid-js"
-import { bus, notify } from "~/utils"
-import { UserMethods, UserPermissions } from "~/types"
+import { bus, convertURL, notify } from "~/utils"
+import { ObjType, UserMethods, UserPermissions } from "~/types"
 import { getSettingBool, haveSelected, me, oneChecked } from "~/store"
+import { players } from "../previews/video_box"
+import { BsPlayCircleFill } from "solid-icons/bs"
 
 const ItemContent = (props: { name: string }) => {
   const t = useT()
@@ -32,6 +34,7 @@ export const ContextMenu = () => {
   const canPackageDownload = () => {
     return UserMethods.is_admin(me()) || getSettingBool("package_download")
   }
+  const { rawLink } = useLink()
   return (
     <Menu
       id={1}
@@ -80,6 +83,46 @@ export const ContextMenu = () => {
         >
           <ItemContent name="download" />
         </Item>
+        <Submenu
+          hidden={({ props }) => {
+            return props.type !== ObjType.VIDEO
+          }}
+          label={
+            <HStack spacing="$2">
+              <Icon
+                as={BsPlayCircleFill}
+                boxSize="$7"
+                p="$0_5"
+                color="$info9"
+              />
+              <Text>{t("home.preview.play_with")}</Text>
+            </HStack>
+          }
+        >
+          <For each={players}>
+            {(player) => (
+              <Item
+                onClick={({ props }) => {
+                  const href = convertURL(player.scheme, {
+                    raw_url: "",
+                    name: props.name,
+                    d_url: rawLink(props, true),
+                  })
+                  window.open(href, "_self")
+                }}
+              >
+                <HStack spacing="$2">
+                  <Image
+                    m="0 auto"
+                    boxSize="$7"
+                    src={`${window.__dynamic_base__}/images/${player.icon}.webp`}
+                  />
+                  <Text>{player.name}</Text>
+                </HStack>
+              </Item>
+            )}
+          </For>
+        </Submenu>
       </Show>
       <Show when={!oneChecked() && haveSelected()}>
         <Submenu label={<ItemContent name="copy_link" />}>
@@ -108,8 +151,8 @@ export const ContextMenu = () => {
             <Item onClick={playlistDownloadSelected}>
               {t("home.toolbar.playlist_download")}
             </Item>
+            <Item onClick={sendToAria2}>{t("home.toolbar.send_aria2")}</Item>
           </Show>
-          <Item onClick={sendToAria2}>{t("home.toolbar.send_aria2")}</Item>
         </Submenu>
       </Show>
     </Menu>
