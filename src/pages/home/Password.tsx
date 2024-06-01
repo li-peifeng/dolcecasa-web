34d0eec296d8cf15308ec5d66330904f70287df2
SweetHome <103113557+li-peifeng@users.noreply.github.com>
2024-06-01 23:46:20 +0200

    更新 Password.tsx

--- src/pages/home/Password.tsx
+++ src/pages/home/Password.tsx
@@ -45,8 +45,8 @@ const Password = () => {
           direction={{ "@initial": "column", "@sm": "row" }}
           columnGap="$1"
         >
-          <Text color="$info9" as={Link} href={`https://peifeng.li/messages`}>
-            点此获取密码
+          <Text color="$info9" as={Link} href={`https://peifeng.li/password`}>
+            获取授权
           </Text>
         </Flex>
         <HStack spacing="$2">
